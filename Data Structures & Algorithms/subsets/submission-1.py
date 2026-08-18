fb6d2d17cbc88633e46416273ca02ad9ca6d90dd
ismilesen <116997613+ismilesen@users.noreply.github.com>
2026-08-17 19:36:58 -0700

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.py
@@ -0,0 +1,7 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        for i in range(len(nums) + 1):
+            for combo in combinations(nums, i):
+                result.append(list(combo))
+        return result
